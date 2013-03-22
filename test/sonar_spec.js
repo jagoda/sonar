867205b993acdc31c390caac7eb648224e68f1cd
Jeff Jagoda <jsjagoda@us.ibm.com>
2013-03-22 12:27:31 -0400

    Implemented ability to collect response bodies.

--- test/sonar_spec.js
+++ test/sonar_spec.js
@@ -1,4 +1,5 @@
 var application = require("./application"),
+    captureData = require("./streams").captureData,
     expect      = require("chai").expect,
     sonar       = require("../");
 
@@ -6,14 +7,31 @@ describe("A Sonar instance", function () {
 
     var app = application();
 
-    it("can retrieve plain text content", function (done) {
-        sonar(app).get("/plain-text", function (response) {
+    it("can capture a response body", function (done) {
+        sonar(app).get("/plain-text", function (error, response) {
+            expect(error).to.be.null;
             expect(response).to.have.property(
                 "body",
                 "This is some plain text."
             );
             done();
         });
+    });
+    
+    it("can be configured to allow manual response parsing", function (done) {
+        function checkBody (error, data) {
+            expect(error).to.be.null;
+            expect(data).to.deep.equal([ "This is some plain text." ]);
+            done();
+        }
+    
+        sonar(app, { parseBody: false })
+            .get("/plain-text", function (error, response) {
+                expect(error).to.be.null;
+                expect(response).to.not.have.property("body");
+                response.setEncoding("utf-8");
+                captureData(response, checkBody);
+            });
     });
 
 });
